93d5eec61a17412c29cee741f4e3d44846d66757
Osobu A Samuel <54308030+aosobu@users.noreply.github.com>
2021-10-04 15:35:19 +0100

    tests publisher-consumer two way communication

---
+++ src/main/java/com/pangaea/consumer/service/KafkaEventListener.java
@@ -0,0 +1,16 @@
+package com.pangaea.consumer.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaEventListener {
+
+    @KafkaListener(topics = "${kafka.topic}")
+    public void listen(Message<String> message){
+        String payload = message.getPayload();
+        System.out.println(payload);
+    }
+
+}
